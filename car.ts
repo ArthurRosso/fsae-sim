2ef65d17cd85cb28979eb1016258b6160cc74e51
ArthurRosso <arthurderosso@gmail.com>
2020-07-23 23:46:18 -0300

    add car class

---
+++ car.ts
@@ -0,0 +1,20 @@
+/*
+massa; 
+distância entreos eixos dianteiro e traseiro; 
+distância entre o eixo dianteiro e o centro de gravidade na direção longitudinal; 
+altura do centro de gravidade; 
+área frontal do veículo; 
+distância vertical padrão entre eixo dianteiro e o solo; 
+distância vertical padrão entre eixo traseiro e o solo; 
+distância entre as linhas de centro dos pneus dianteiros na direção longitudinal;
+distância entre as linhas de centro dos pneus traseiros na direção longitudinal; 
+constantes elásticas das molas das suspensões dianteira e traseira; 
+razão de movimento das molas das suspensões dianteira e traseira; 
+constantes elásticas das barras antirolagem das suspensões dianteira e traseira; 
+razão de movimento das barras antirolagem das suspensões dianteira e traseira; 
+constante elástica do pneu; 
+distância vertical entre o centro de rolagem da suspensão dianteira e o solo; 
+distância vertical entre o centro de rolagem da suspensão traseira e o solo.
+
+Página 39
+*/
